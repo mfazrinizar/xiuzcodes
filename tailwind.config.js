b14d425407b0bf6f472f53589a193a77bb222645
asmin-dev <asmin987asmin@gmail.com>
2021-06-22 06:50:11 +0800

    purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.vue'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
